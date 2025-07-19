fb5119801595c7adfd353626573828dd09300bfa
Ryota <ryotassoccer19@gmail.com>
2025-07-19 16:43:28 +0900

    Fix ALLOWED_HOSTS env key typo

--- nagoyameshi_django_api/config/settings.py
+++ nagoyameshi_django_api/config/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # ── 環境変数から取得
 SECRET_KEY            = env('SECRET_KEY')
 DEBUG                 = env('DEBUG')
-ALLOWED_HOSTS         =env.list(' ALLOWED_HOSTS')
+ALLOWED_HOSTS         =env.list('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
