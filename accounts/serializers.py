ab2f409edd8ffc26a8e9147434e5f7ad11db3b63
Ryota <ryotassoccer19@gmail.com>
2025-07-20 10:08:27 +0900

    fix: CustomUser/serializer/settings/マイグレーション修正

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from dj_rest_auth.registration.serializers import RegisterSerializer
-
 
 User = get_user_model()
 
 class CustomUserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
-
+        fields = ('id', 'username', 'email', 'plan', 'stripe_customer_id', 'stripe_subscription_id')
