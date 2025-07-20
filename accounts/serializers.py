da3946ba017787f3ed3c2021a7d0225580e34e30
Ryota <ryotassoccer19@gmail.com>
2025-07-20 10:23:41 +0900

    fix: RegisterSerializer追加

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,15 +4,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class CustomRegisterSerializer(RegisterSerializer):
+    def custom_signup(self, request, user):
+        pass
+
 class CustomUserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'plan', 'stripe_customer_id', 'stripe_subscription_id')
-
-class CustomRegisterSerializer(RegisterSerializer):
-    # 追加フィールドを書く（今回は無くてもOK。phone_numberを追加したい場合の例↓）
-    # phone_number = serializers.CharField(required=False)
-
-    def custom_signup(self, request, user):
-        # カスタムサインアップ処理（現状は不要）
-        pass
