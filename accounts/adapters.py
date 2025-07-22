fb00de67d9ec060b9342fe87cb3d66de546baca7
Ryota <ryotassoccer19@gmail.com>
2025-07-22 09:02:46 +0900

    メール送信メソッド修正

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def send_confirmation_mail(self, request, emailconfirmation, signup):
+        # Heroku環境変数 FRONTEND_URL を参照
         activate_url = f"{settings.FRONTEND_URL}/confirm-email/?key={emailconfirmation.key}"
         ctx = {
             "user": emailconfirmation.email_address.user,
@@ -10,7 +11,6 @@ class CustomAccountAdapter(DefaultAccountAdapter):
             "current_site": None,
             "key": emailconfirmation.key,
         }
-        # ★ ここが正しい呼び出し方（template_prefix, email, context のみ！）
         self.send_mail(
             'account/email/email_confirmation',
             emailconfirmation.email_address.email,
